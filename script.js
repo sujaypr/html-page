a6511c9de5f8e44d7b166a5c5f499c27d199fbe1
sujay <sujayprasad549@gmail.com>
2023-02-18 18:32:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function menuopen(){
+    var x=document.querySelector(".nav-links");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+};
+
+function menuclose(){
+    var x=document.querySelector(".nav-links");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+};
